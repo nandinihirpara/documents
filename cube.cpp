04840994115a14fce0312ef63a935978218acec0
nandinihirpara <119646455+nandinihirpara@users.noreply.github.com>
2023-01-11 10:36:55 +0530

    Add files via upload

---
+++ cube.cpp
@@ -0,0 +1,25 @@
+// cubic multiply value program
+#include <iostream>
+using namespace std;
+class line
+{
+public:
+    inline float mul(float m, float n)
+    {
+        return (m * n);
+    }
+    {
+        inline float(m * n * n);
+    }
+};
+int main()
+{
+    line obj;
+    float value1, value2;
+    cout << "\n enate 1st value :=";
+    cin >> value1;
+    cout << "\n entare 2nd value :=";
+    cin >> value2;
+    cout << "\n multiply valueis :=" << obj.mul(value1, value2);
+    return 0;
+}
